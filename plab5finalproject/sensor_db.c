ab0b2ba63b4ea0320be7402f746372ae654d6ba1
yewentai <yewentai@foxmail.com>
2022-12-23 10:00:56 +0100

    1

--- plab5finalproject/sensor_db.c
+++ plab5finalproject/sensor_db.c
@@ -1,6 +1,5 @@
 #include "sensor_db.h"
 
-char buffer[SIZE];
 FILE *open_db(char *filename, bool append);
 int insert_sensor(FILE *f, sensor_data_t *data);
 int close_db(FILE *f);
@@ -8,16 +7,16 @@ int close_db(FILE *f);
 void *storagemgr()
 {
     FILE *f = open_db("sensor_data.csv", false); // Create sensor_data.csv
-    strcpy(buffer, "A new data.csv file has been created.");
-    write(fd[WRITE_END], buffer, SIZE);
+    strcpy(log_msg, "A new data.csv file has been created.");
+    write(fd[WRITE_END], log_msg, SIZE);
     sensor_data_t *data = malloc(sizeof(sensor_data_t));
     while (sbuffer_remove(sbuffer, data) != SBUFFER_FAILURE)
     {
         insert_sensor(f, data);
     }
     close_db(f); // Close sensor_data.csv
-    strcpy(buffer, "The data.csv file has been closed.");
-    write(fd[WRITE_END], buffer, SIZE);
+    strcpy(log_msg, "The data.csv file has been closed.");
+    write(fd[WRITE_END], log_msg, SIZE);
     free(data);
 }
 
@@ -31,14 +30,14 @@ FILE *open_db(char *filename, bool append)
     if (fp == NULL)
     {
         perror("fopen()");
-        strcpy(buffer, "Failed to open the sensor_date file");
-        write(fd[WRITE_END], buffer, SIZE);
+        strcpy(log_msg, "Failed to open the sensor_date file");
+        write(fd[WRITE_END], log_msg, SIZE);
         exit(1);
     }
     else
     {
-        strcpy(buffer, "Successfully opened the sensor_date file");
-        write(fd[WRITE_END], buffer, SIZE);
+        strcpy(log_msg, "Successfully opened the sensor_date file");
+        write(fd[WRITE_END], log_msg, SIZE);
     }
     return fp;
 }
@@ -55,8 +54,8 @@ int insert_sensor(FILE *f, sensor_data_t *data)
 
     strftime(time_buffer, sizeof(time_buffer), "%d/%m/%Y %H:%M:%S", localtime(&data->ts));
     fprintf(f, "%s,%" PRIu16 ",%.1lf\n", time_buffer, data->id, data->value);
-    sprintf(buffer, "Data insertion from sensor %d succeeded.", data->id);
-    write(fd[WRITE_END], buffer, SIZE);
+    sprintf(log_msg, "Data insertion from sensor %d succeeded.", data->id);
+    write(fd[WRITE_END], log_msg, SIZE);
     exit(EXIT_SUCCESS);
 }
 
@@ -68,7 +67,7 @@ int close_db(FILE *f)
         exit(1);
     }
     fclose(f);
-    strcpy(buffer, "Successfully closed the sensor_date file");
-    write(fd[WRITE_END], buffer, SIZE);
+    strcpy(log_msg, "Successfully closed the sensor_date file");
+    write(fd[WRITE_END], log_msg, SIZE);
     exit(EXIT_SUCCESS);
 }
