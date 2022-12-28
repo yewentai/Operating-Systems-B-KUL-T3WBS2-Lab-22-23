e1886b34e84992a08d76debc7f9429ea884e1fde
yewentai <yewentai@foxmail.com>
2022-12-28 15:59:40 +0100

    1

--- lab_final/lib/dplist.h
+++ lab_final/lib/dplist.h
@@ -5,8 +5,10 @@
 #ifndef _DPLIST_H_
 #define _DPLIST_H_
 
-typedef enum {
-    false, true
+typedef enum
+{
+    false,
+    true
 } bool; // or use C99 #include <stdbool.h>
 
 /**
@@ -28,10 +30,9 @@ typedef struct dplist_node dplist_node_t;
  * \return a pointer to a newly-allocated and initialized list.
  */
 dplist_t *dpl_create(
-        void* (*element_copy)(void *element),
-        void (*element_free)(void **element),
-        int (*element_compare)(void *x, void *y)
-);
+    void *(*element_copy)(void *element),
+    void (*element_free)(void **element),
+    int (*element_compare)(void *x, void *y));
 
 /** Deletes all elements in the list
  * - Every list node of the list needs to be deleted. (free memory)
@@ -120,5 +121,4 @@ int dpl_get_index_of_element(dplist_t *list, void *element);
  */
 void *dpl_get_element_at_reference(dplist_t *list, dplist_node_t *reference);
 
-#endif  // _DPLIST_H_
-
+#endif // _DPLIST_H_
